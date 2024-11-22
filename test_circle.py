b7d799e3a712906cef9ad4cf4899f508ad13eb82
A1bic <leokov3009@gmail.com>
2024-11-22 20:00:01 +0000

    Subtests added

--- test_circle.py
+++ test_circle.py
@@ -1,29 +1,37 @@
 import unittest
 from circle import area, perimeter
 
-class RectangleTestCase(unittest.TestCase):
+class CircleTestCase(unittest.TestCase):
     '''
     Тесты для area()
     '''
 
     #Базовые тесты
     def test_area_basic(self):
-        self.assertEqual(area(4), 50.26548245743669)
-        self.assertEqual(area(145), 66051.9855417254)
-        self.assertEqual(area(12), 452.3893421169302)
+        with self.subTest("Regular Test 1"):
+            self.assertEqual(area(4), 50.26548245743669)
+        with self.subTest("Regular Test 2"):
+            self.assertEqual(area(145), 66051.9855417254)
+        with self.subTest("Regular Test 3"):
+            self.assertEqual(area(12), 452.3893421169302)
         
 
 
     #Тестирование с нулями
     def test_area_zero(self):
-        self.assertEqual(area(0), 0)
+        with self.subTest("Zero Test"):
+            self.assertEqual(area(0), 0)
 
     #Тестирование граничных чисел
     def test_area_limit_value(self):
-        self.assertEqual(area(12345678), 478828248493921.5)
-        self.assertEqual(area(0.01), 0.00031415926535897936)
-        self.assertIsInstance(area(1e18), float)
-        self.assertIsInstance(area(1e18 ** -1), float)
+        with self.subTest("Big number Test"):
+            self.assertEqual(area(12345678), 478828248493921.5)
+        with self.subTest("Small number Test"):
+            self.assertEqual(area(0.01), 0.00031415926535897936)
+        with self.subTest("Very big number type Test"):
+            self.assertIsInstance(area(1e18), float)
+        with self.subTest("Very small number type Test"):
+            self.assertIsInstance(area(1e18 ** -1), float)
 
 
 
@@ -33,35 +41,47 @@ class RectangleTestCase(unittest.TestCase):
 
     #Базовые тесты
     def test_perimeter_basic(self):
-        self.assertEqual(perimeter(5), 31.41592653589793)
-        self.assertEqual(perimeter(234), 1470.2653618800232)
-        self.assertEqual(perimeter(12), 75.39822368615503)
+        with self.subTest("Regular Test 1"):
+            self.assertEqual(perimeter(5), 31.41592653589793)
+        with self.subTest("Regular Test 2"):
+            self.assertEqual(perimeter(234), 1470.2653618800232)
+        with self.subTest("Regular Test 3"):
+            self.assertEqual(perimeter(12), 75.39822368615503)
 
     #Тестирование с нулями
     def test_perimeter_zero(self):
-        self.assertEqual(perimeter(0), 0)
+        with self.subTest("Zero Test"):
+            self.assertEqual(perimeter(0), 0)
         
     #Тестирование граничных чисел
     def testperimeter_limit_value(self):
-        self.assertEqual(perimeter(87654321), 550748341.818003)
-        self.assertEqual(perimeter(0.0001), 0.0006283185307179586)
-        self.assertIsInstance(perimeter(1e18), float)
-        self.assertIsInstance(perimeter(1e18 ** -1), float)
+        with self.subTest("Big number Test"):
+            self.assertEqual(perimeter(87654321), 550748341.818003)
+        with self.subTest("Small number Test"):
+            self.assertEqual(perimeter(0.0001), 0.0006283185307179586)
+        with self.subTest("Very big number type Test"):
+            self.assertIsInstance(perimeter(1e18), float)
+        with self.subTest("Very small number type Test"):
+            self.assertIsInstance(perimeter(1e18 ** -1), float)
 
 
     '''
     Проверка вводимых данных
     '''
     def test_insert_value_errors(self):
-        with self.assertRaises(TypeError):
-            area("5")
+        with self.subTest("TypeError Area Test"):
+            with self.assertRaises(TypeError):
+                area("5")
         
-        with self.assertRaises(ValueError):
-            area(-3)
+        with self.subTest("ValueError Area Test"):
+            with self.assertRaises(ValueError):
+                area(-3)
 
-        with self.assertRaises(TypeError):
-            perimeter("4")
+        with self.subTest("TypeError Perimeter Test"):
+            with self.assertRaises(TypeError):
+                perimeter("4")
 
-        with self.assertRaises(ValueError):
-            perimeter(-5)
+        with self.subTest("ValueError Perimeter Test"):
+            with self.assertRaises(ValueError):
+                perimeter(-5)
 
--- test_square.py
+++ test_square.py
@@ -1,7 +1,7 @@
 import unittest
 from square import area, perimeter
 
-class RectangleTestCase(unittest.TestCase):
+class SquareTestCase(unittest.TestCase):
 
     '''
     Тесты для area()
@@ -9,22 +9,31 @@ class RectangleTestCase(unittest.TestCase):
 
     #Базовые тесты
     def test_area_basic(self):
-        self.assertEqual(area(10), 100)
-        self.assertEqual(area(5), 25)
-        self.assertEqual(area(18), 324)
-        self.assertEqual(area(8), 64)
+        with self.subTest("Regular Test 1"):
+            self.assertEqual(area(10), 100)
+        with self.subTest("Regular Test 2"):
+            self.assertEqual(area(5), 25)
+        with self.subTest("Regular Test 3"):
+            self.assertEqual(area(18), 324)
+        with self.subTest("Regular Test 4"):
+            self.assertEqual(area(8), 64)
 
 
     #Тестирование с нулями
     def test_area_zero(self):
-        self.assertEqual(area(0), 0)
+        with self.subTest("Zero Test"):
+            self.assertEqual(area(0), 0)
 
     #Тестирование граничных чисел
     def test_area_limit_value(self):
-        self.assertEqual(area(9999999), 9999999**2)
-        self.assertEqual(area(0.00001), 0.00001**2)
-        self.assertIsInstance(area(1e18), float)
-        self.assertIsInstance(area(1e18 ** -1), float)
+        with self.subTest("Big number Test"):
+            self.assertEqual(area(9999999), 9999999**2)
+        with self.subTest("Small number Test"):
+            self.assertEqual(area(0.00001), 0.00001**2)
+        with self.subTest("Very big number type Test"):
+            self.assertIsInstance(area(1e18), float)
+        with self.subTest("Very small number type Test"):
+            self.assertIsInstance(area(1e18 ** -1), float)
 
     '''
     Тесты для perimeter()
@@ -32,30 +41,43 @@ class RectangleTestCase(unittest.TestCase):
 
     #Базовые тесты
     def test_perimeter_basic(self):
-        self.assertEqual(perimeter(10), 40)
-        self.assertEqual(perimeter(47), 188)
-        self.assertEqual(perimeter(1), 4)
-        self.assertEqual(perimeter(5), 20)
+        with self.subTest("Regular Test 1"):
+            self.assertEqual(perimeter(10), 40)
+        with self.subTest("Regular Test 2"):
+            self.assertEqual(perimeter(47), 188)
+        with self.subTest("Regular Test 3"):
+            self.assertEqual(perimeter(1), 4)
+        with self.subTest("Regular Test 4"):
+            self.assertEqual(perimeter(5), 20)
 
     #Тестирование с нулями
     def test_perimeter_zero(self):
-        self.assertEqual(perimeter(0), 0)
+        with self.subTest("Zero Test"):
+            self.assertEqual(perimeter(0), 0)
         
     #Тестирование граничных чисел
     def testperimeter_limit_value(self):
-        self.assertEqual(perimeter(9999999), 39999996)
-        self.assertEqual(perimeter(0.00001), 0.00004)
-        self.assertIsInstance(perimeter(1e18), float)
-        self.assertIsInstance(perimeter(1e18 ** -1), float)
+        with self.subTest("Big number Test"):
+            self.assertEqual(perimeter(9999999), 39999996)
+        with self.subTest("Small number Test"):
+            self.assertEqual(perimeter(0.00001), 0.00004)
+        with self.subTest("Very big number type Test"):
+            self.assertIsInstance(perimeter(1e18), float)
+        with self.subTest("Very small number type Test"):
+            self.assertIsInstance(perimeter(1e18 ** -1), float)
     '''
     Проверка типов данных
     '''
     def test_insert_value_errors(self):
-        with self.assertRaises(ValueError):
-            area(-10)
-        with self.assertRaists(TypeError):
-            area("5")
-        with self.assertRaises(TypeError):
-            perimeter("13")
-        with self.assertRaises(ValueError):
-            perimeter(-3)
+        with self.subTest("TypeError Area Test"):
+            with self.assertRaises(ValueError):
+                area(-10)
+        with self.subTest("ValueError Area Test"):
+            with self.assertRaists(TypeError):
+                area("5")
+        with self.subTest("TypeError Perimeter Test"):
+            with self.assertRaises(TypeError):
+                perimeter("13")
+        with self.subTest("ValueError Perimeter Test"):
+            with self.assertRaises(ValueError):
+                perimeter(-3)
